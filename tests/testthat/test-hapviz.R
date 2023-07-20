249a3bcee8d2ddd47c46a13fd0614c7cf73f903a
Jacob Marsh <51686617+jacobimarsh@users.noreply.github.com>
2023-07-20 14:41:15 -0400

    rebuild unit tests for v1.3.0

--- tests/testthat/test-hapviz.R
+++ tests/testthat/test-hapviz.R
@@ -9,21 +9,21 @@ testHapObject <- run_haplotyping(vcf = crosshap::vcf,
                           epsilon = test_epsilon)
 
 test_that("test crosshap viz", {
-  haplotype_viz2 <- crosshap_viz(HapObject = testHapObject, epsilon = 0.61)
-  vdiffr::expect_doppelganger("haplotype_viz2", haplotype_viz2)
+  haplotype_viz3 <- crosshap_viz(HapObject = testHapObject, epsilon = 0.61)
+  vdiffr::expect_doppelganger("haplotype_viz3", haplotype_viz3)
 })
 
 test_that("test alt crosshap viz", {
-alt_viz2 <- crosshap_viz(HapObject = testHapObject, epsilon = test_epsilon, plot_left = 'pos', plot_right = 'cluster')
-vdiffr::expect_doppelganger("haplotype_viz_alt2", alt_viz2)
+alt_viz3 <- crosshap_viz(HapObject = testHapObject, epsilon = test_epsilon, plot_left = 'pos', plot_right = 'cluster')
+vdiffr::expect_doppelganger("haplotype_viz_alt3", alt_viz3)
 })
 
 test_that("test no labels crosshap viz", {
-nolabs <- crosshap_viz(HapObject = testHapObject, epsilon = test_epsilon, hide_labels = TRUE)
-vdiffr::expect_doppelganger("haplotype_viz_nolabs", nolabs)
+nolabs2 <- crosshap_viz(HapObject = testHapObject, epsilon = test_epsilon, hide_labels = TRUE)
+vdiffr::expect_doppelganger("haplotype_viz_nolabs2", nolabs2)
 })
 
 test_that("test isolate_groups", {
-isolate_wt <- crosshap_viz(HapObject = testHapObject, epsilon = test_epsilon, isolate_group = "wt")
-vdiffr::expect_doppelganger("haplotype_viz_isolatewt", isolate_wt)
+isolate_wt2 <- crosshap_viz(HapObject = testHapObject, epsilon = test_epsilon, isolate_group = "wt")
+vdiffr::expect_doppelganger("haplotype_viz_isolatewt2", isolate_wt2)
 })
