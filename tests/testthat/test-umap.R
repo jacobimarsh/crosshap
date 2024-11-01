cbd0b336d720298443ac3ad0d6b2ac17b049d9e4
Jacob Marsh <jake.marsh@live.com.au>
2024-11-01 11:10:32 -0400

    re-test

--- tests/testthat/test-umap.R
+++ tests/testthat/test-umap.R
@@ -1,7 +1,7 @@
 set.seed(153)
 
 umap_in <- umap::umap(crosshap::LD, min_dist = 2, spread = 2.5, n_neighbors = 30)
-test_anim_gg2 <- prepare_hap_umap(umap_in,
+test_anim_gg2 <- crosshap::prepare_hap_umap(umap_in,
                                   HapObject = crosshap::HapObject,
                                   epsilon = 0.6,
                                   vcf = crosshap::vcf,
